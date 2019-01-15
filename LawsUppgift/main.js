2aa333c41f33c8520b3ce01b0ba92abdcea36f76
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2019-01-15 14:30:56 +0100

    removed laws

--- LawsUppgift/main.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function () {
-    document.getElementById("mainpage-decor-bar").classList.remove("mainpage-decor-bar-hidden");
-    document.getElementById("mainpage-text").classList.remove("mainpage-text-hidden");
-    document.body.style.overflowY = "auto";
-}, 500);
-window.scrollTo(0, 0);
