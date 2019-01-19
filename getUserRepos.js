96cb84045e62a58fa7ff6ab3b439c3f2b5a7a183
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2019-01-19 13:47:13 +0100

    Made the repo thing a slidedown

--- getUserRepos.js
+++ getUserRepos.js
@@ -15,7 +15,19 @@ function createUserRepos(jsonFile) {
             var repoDiv = document.createElement("div");
             repoDiv.classList.add("repo-card");
             repoDiv.innerHTML = '<a class="repo-card-title" href="' + jsonFile[repo].html_url + '">' + jsonFile[repo].name + '</a><br/><br/><a>' + jsonFile[repo].description + '</a><a class="repo-card-language">' + jsonFile[repo].language + '</a><br/><br/><div><a><i class="fas fa-star"></i>' + jsonFile[repo].stargazers_count + '</a> <a> <i class="fas fa-eye"></i>' + jsonFile[repo].watchers_count + '</a></div>';
-            document.body.appendChild(repoDiv);
+            document.getElementById("repos").appendChild(repoDiv);
         }
     }
+}
+
+function showHideUserRepos() {
+    if (document.getElementById("repos").style.display == "none") {
+        document.getElementById("repos").style.display = "inline";
+        document.getElementById("hide-repos-button").innerHTML = "<i class='fas fa-arrow-up'></i>";
+        document.getElementById("hide-repos-text").innerText = "My repos - hide repos";
+    } else {
+        document.getElementById("repos").style.display = "none";
+        document.getElementById("hide-repos-button").innerHTML = "<i class='fas fa-arrow-down'></i>";
+        document.getElementById("hide-repos-text").innerText = "My repos - show repos";
+    };
 }
