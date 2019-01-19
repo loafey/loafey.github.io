ea57f73564bfa92f49e56ad9c293933fc474a166
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2019-01-19 01:23:14 +0100

    Started generating repo info

--- populateAccount.js
+++ populateAccount.js
@@ -1,14 +1,7 @@
 var githubUserLink = "https://api.github.com/users/samhamnam";
 var request = new XMLHttpRequest();
+request.open("GET", githubUserLink);
 
-var debugJson = false;
-
-if (debugJson != true) {
-    request.open("GET", githubUserLink);
-} else {
-    request.open("GET", "./temporary_info.json");
-    console.log("Loaded temporary json file");
-}
 request.responseType = "json";
 request.send();
 request.onload = function () {
