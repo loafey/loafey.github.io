62bc202d5760f555a0421c03e6754a8d39116ed9
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-01-30 16:26:36 +0100

    Fix.

--- resources/projects.js
+++ resources/projects.js
@@ -2,52 +2,50 @@ const PROJECT_LIST = [
     {
         title: "Bachelor Thesis",
         language: "Haskell",
+        link: "https://github.com/samhamnam/churf",
         content: `A typed functional programming language based on System-F. Created by a
-        team of six people, <br>
+        team of six people, 
         and I personally spent time working on the
-        code generation to LLVM IR.
-
-        <a href="https://github.com/samhamnam/churf">Link to repo with thesis</a>.`
+        code generation to LLVM IR.`
     },
     {
         title: "Advent of Code", language: "Rust",
-        content: `My solutions of Advent of Code in Rust, <a
-                    href =\"https://github.com/samhamnam/advent_of_code\">link to repo</a>.`,
+        link: "https://github.com/samhamnam/advent_of_code",
+        content: `My solutions of Advent of Code in Rust.`
     },
     {
         title: "rusty_procedure_call", language: "Rust",
+        link: "https://github.com/samhamnam/rusty_procedure_call",
         content: `A proof of concept RPC macro for Rust.
         Automatically generates the RPC network code
-        for a <br> struct allowing for easy prototyping for
-        network interfaces.
-
-        <a href="https://github.com/samhamnam/rusty_procedure_call">Link to repo.</a>`
+        for a  struct allowing for easy prototyping for
+        network interfaces.`
     },
     {
         title: "Interior Mutability Pointer", language: "Rust",
+        link: "https://github.com/samhamnam/interior_mutability_pointer",
         content: `A highly experimental library for Rust adding a smart pointer which gives you a
-        similar pointer<br>
+        similar pointer
         experience to Java/C#, letting you avoid the cumbersome syntax of
         <code>Rc&lt;RefCell&lt;T&gt;&gt;</code>.
-        <br>
+        
         Actually pretty <a
             href="https://github.com/samhamnam/interior_mutability_pointer/issues/2">unsafe</a>
         but a pretty neat concept.`
     },
     {
         title: "dvet.se", language: "JavaScript",
-        content: `The site for our student organization. <br>
-        The site can be found here at <a href="https://dvet.se">dvet.se</a>,
-        and the repository for the site can be found <a
-            href="https://github.com/Datavetenskapsdivisionen/dvet.se">here</a>.`
+        link: "https://github.com/Datavetenskapsdivisionen/dvet.se",
+        content: `The site for our student organization. 
+        The site can be found here at <a href="https://dvet.se">dvet.se</a>.`
     },
     {
         title: "NixOS config", language: "Nix",
-        content: `My NixOS config for the nerds.
-        <a href="https://github.com/samhamnam/niiix">Link to repo</a>.`
+        link: "https://github.com/samhamnam/niiix",
+        content: `My NixOS config for the nerds.`
     },
     {
-        title: "The rest",
+        title: "The rest", language: "", link: null,
         content: `The rest of my projects can be found on my <a href="https://github.com/samhamnam">GitHub
         page</a>.`
     },
