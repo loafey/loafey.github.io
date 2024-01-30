daf53ad8bd68cf0c4e5078cce027854f40a1a68b
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-01-30 12:57:01 +0100

    Reworked how projects are displayed.

---
+++ resources/projects.js
@@ -0,0 +1,54 @@
+const PROJECT_LIST = [
+    {
+        title: "Bachelor Thesis",
+        language: "Haskell",
+        content: `A typed functional programming language based on System-F. Created by a
+        team of six people, <br>
+        and I personally spent time working on the
+        code generation to LLVM IR.
+
+        <a href="https://github.com/samhamnam/churf">Link to repo with thesis</a>.`
+    },
+    {
+        title: "Advent of Code", language: "Rust",
+        content: `My solutions of Advent of Code in Rust, <a
+                    href =\"https://github.com/samhamnam/advent_of_code\">link to repo</a>.`,
+    },
+    {
+        title: "rusty_procedure_call", language: "Rust",
+        content: `A proof of concept RPC macro for Rust.
+        Automatically generates the RPC network code
+        for a <br> struct allowing for easy prototyping for
+        network interfaces.
+
+        <a href="https://github.com/samhamnam/rusty_procedure_call">Link to repo.</a>`
+    },
+    {
+        title: "Interior Mutability Pointer", language: "Rust",
+        content: `A highly experimental library for Rust adding a smart pointer which gives you a
+        similar pointer<br>
+        experience to Java/C#, letting you avoid the cumbersome syntax of
+        <code>Rc&lt;RefCell&lt;T&gt;&gt;</code>.
+        <br>
+        Actually pretty <a
+            href="https://github.com/samhamnam/interior_mutability_pointer/issues/2">unsafe</a>
+        but a pretty neat concept.`
+    },
+    {
+        title: "dvet.se", language: "JavaScript",
+        content: `The site for our student organization. <br>
+        The site can be found here at <a href="https://dvet.se">dvet.se</a>,
+        and the repository for the site can be found <a
+            href="https://github.com/Datavetenskapsdivisionen/dvet.se">here</a>.`
+    },
+    {
+        title: "NixOS config", language: "Nix",
+        content: `My NixOS config for the nerds.
+        <a href="https://github.com/samhamnam/niiix">Link to repo</a>.`
+    },
+    {
+        title: "The rest",
+        content: `The rest of my projects can be found on my <a href="https://github.com/samhamnam">GitHub
+        page</a>.`
+    },
+];
