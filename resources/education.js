0be13819ef63645e879f98e8573180f3fa1de85d
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-01-30 20:45:29 +0100

    Finished CV

---
+++ resources/education.js
@@ -0,0 +1,17 @@
+const EDUCATION = [
+    {
+        title: "Computer Science",
+        subtitle: "Bachelor - University of Gothenburg",
+        date: "August 2020 - June 2023",
+        description: `
+        The courses were mainly focused on programming and mathematics:
+        <ul>
+            <li>Object Oriented & Functional programming</li>
+            <li>Project work in larger teams</li>
+            <li>Computer networking, parallel & concurrent programming</li>
+            <li>Data structures & algorithms</li>
+            <li>Programming language technology & compilers</li>
+        </ul
+        `
+    }
+];
