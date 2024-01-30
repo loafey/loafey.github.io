bc331b962ce05057880eb36c708b5abd1f1ef606
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-01-30 16:52:59 +0100

    Started autogenerating the CV.

---
+++ resources/experiences.js
@@ -0,0 +1,18 @@
+const EXPERIENCES = [
+    {
+        title: "Aptiv",
+        subtitle: "Student Engineer",
+        date: "January 2022 - August 2022",
+        description: `
+        Worked with the R&D team. Mainly focused on improving the CI pipeline.
+        Mainly worked with Rust, but also got to work with FFI and software such as MatLab.`
+    },
+    {
+        title: "Chalmers",
+        subtitle: "Teacher Assistant",
+        date: "2021-2023 - Multiple occasions",
+        description: `
+        Worked as a TA multiple times in beginner courses in Python, 
+        and once in a beginner course in functional programming.`
+    }
+];
---
+++ resources/other-experiences.js
@@ -0,0 +1,10 @@
+const OTHER_EXPERIENCES = [
+    {
+        title: "Computer Science Division",
+        subtitle: "Chairman of the Board",
+        date: "September 2021 - Present",
+        description: `
+        Acting president of <a href="https://dvet.se">Computer Science Division student organization</a>, for the Computer Science
+        bachelor and master program, and the Applied Data Science master program at the University of Gothenburg.`
+    }
+];
