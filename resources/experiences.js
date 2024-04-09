081db04fddf143c47d6e562c834cfbeebb865bdb
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-04-09 11:44:30 +0200

    Update CV.

--- resources/experiences.js
+++ resources/experiences.js
@@ -1,11 +1,13 @@
 const EXPERIENCES = [
     {
-        title: "Aptiv",
-        subtitle: "Student Engineer",
-        date: "January 2022 - August 2022",
+        title: "Chalmers",
+        subtitle: "Research Assistant",
+        date: "January 2024 - July 2024",
         description: `
-        Worked with the R&D team. Mainly focused on improving the CI pipeline.
-        Mainly worked with Rust, but also got to work with FFI and software such as MatLab.`
+        Worked as research assistant for <a href="https://www.cse.chalmers.se/~koen/">Koen Claessen</a> 
+        and <a href="https://research.chalmers.se/en/person/krookr">Robert Krook</a>. 
+        Mainly focused on creating samples of distributed programs in Haskell for use with CPU enclaves.
+        `
     },
     {
         title: "Chalmers",
@@ -14,5 +16,13 @@ const EXPERIENCES = [
         description: `
         Worked as a TA multiple times in beginner courses in Python, 
         and once in a beginner course in functional programming.`
+    },
+    {
+        title: "Aptiv",
+        subtitle: "Student Engineer",
+        date: "January 2022 - August 2022",
+        description: `
+        Worked with the R&D team. Mainly focused on improving the CI pipeline.
+        Mainly worked with Rust, but also got to work with FFI and software such as MatLab.`
     }
 ];
